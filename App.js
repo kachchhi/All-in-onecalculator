777bfbfa6847691540af1d1f700d58f271d8b6a1
kachchhi <55308892+kachchhi@users.noreply.github.com>
2019-12-18 17:10:39 +0530

    some change project

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, StatusBar} from 'react-native';
+import {} from 'react-native';
 
-import HomeHeader from './HomeHeader';
 import SipCalculator from './SipCalculator';
-import SipBody from './SipBody';
-import OneCalculator from './OneCalculator';
+
+// import OneCalculator from './OneCalculator';
 
 import {Container} from 'native-base';
 
@@ -12,24 +11,9 @@ export default class App extends React.Component {
   render() {
     return (
       <Container>
-        <HomeHeader />
-        <OneCalculator />
+        {/* <OneCalculator /> */}
         <SipCalculator />
-        <SipBody />
       </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'skyblue',
-    height: '100%',
-  },
-  paragraph: {
-    fontSize: 10,
-    fontWeight: 'bold',
-
-    flex: 1,
-  },
-});
--- SipCalculator.js
+++ SipCalculator.js
@@ -21,21 +21,68 @@ import {
   FooterTab,
   Input,
   ListItem,
+  List,
 } from 'native-base';
 
 export default class SipCalculator extends React.Component {
   render() {
     return (
-      <Footer style={{marginTop: 10}}>
-        <FooterTab>
-          <Button active>
-            <Text style={{fontSize: 15, fontWeight: 'bold'}}>Sip</Text>
-          </Button>
-          <Button active>
-            <Text style={{fontSize: 15, fontWeight: 'bold'}}>One-Time</Text>
-          </Button>
-        </FooterTab>
-      </Footer>
+      <Container>
+        <Header>
+          <Left>
+            <Button>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+
+          <Body>
+            <Title style={{margin: 10}}>SipCalculator</Title>
+          </Body>
+        </Header>
+
+        <Footer style={{marginTop: 10}}>
+          <FooterTab>
+            <Button active>
+              <Text style={{fontSize: 15, fontWeight: 'bold'}}>Sip</Text>
+            </Button>
+            <Button active>
+              <Text style={{fontSize: 15, fontWeight: 'bold'}}>One-Time</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+        <Content>
+          <List>
+            <ListItem>
+              <Body>
+                <Text>Investment Amount</Text>
+                <Text note></Text>
+              </Body>
+              <Right>
+                <Input
+                  style={{borderWidth: 2, width: 150}}
+                  defaultValue={'10000'}
+                />
+              </Right>
+            </ListItem>
+            <ListItem>
+              <Body>
+                <Text>Expected Gian/Year</Text>
+                <Text note></Text>
+              </Body>
+              <Right>
+                <Input
+                  style={{borderWidth: 2, width: 150}}
+                  defaultValue={'21%'}
+                />
+              </Right>
+            </ListItem>
+
+            <Button block rounded success>
+              <Text>calculate</Text>
+            </Button>
+          </List>
+        </Content>
+      </Container>
     );
   }
 }
